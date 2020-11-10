02439015fd0528c53aac859095be52128126093c
Vla-a <vova.liv.84@mail.ru>
2020-11-10 13:47:41 +0300

    cc

--- academy/src/by/homework/homework0/Aplication.java
+++ academy/src/by/homework/homework0/Aplication.java
@@ -2,4 +2,37 @@ package by.homework.homework0;
 
 public class Aplication {
 
+	public static void main(String[] args) {
+		Cat cat1 = new Cat();
+
+		Cat bars = new Cat("bars");
+
+		bars.eat();
+
+		bars.sleep();
+
+		bars.walk();
+
+		cat1.grow(1);
+
+		cat1.grow(4);
+
+		cat1.grow(10);
+
+		int getAge = cat1.getAge();
+		System.out.println(getAge);
+
+		double money = cat1.getMoney();
+		System.out.println(money);
+
+		cat1.setInitials();
+
+		char getInitials = cat1.getInitials();
+		System.out.println(getInitials);
+
+		cat1.isHomeAnimals(true);
+
+		boolean setIsHomeAnimals = cat1.setIsHomeAnimals();
+		System.out.println("�������� �� �������� ��������: " + setIsHomeAnimals);
+	}
 }
