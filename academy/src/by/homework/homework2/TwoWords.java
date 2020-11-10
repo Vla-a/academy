40aa1ac8a71550b2bab541df387b30a44efb2544
Vla-a <vova.liv.84@mail.ru>
2020-11-10 10:49:47 +0300

    Homework2

---
+++ academy/src/by/homework/homework2/TwoWords.java
@@ -0,0 +1,29 @@
+package by.homework.homework2;
+
+import java.util.Scanner;
+
+public class TwoWords {
+
+	public static void main(String[] args) {
+		int n1;
+		Scanner sc = new Scanner(System.in);
+
+		String A = sc.next();
+		String B = sc.next();
+		int n = A.length();
+		int n2 = B.length();
+		int n3;
+		if (n % 2 == 0 && n2 % 2 == 0) {
+			n = n;
+			n1 = n / 2;
+			n2 = n2;
+			n3 = n2 / 2;
+			String substr1 = A.substring(0, n1);
+			String substr2 = B.substring(n3, n2);
+			System.out.print(substr1 + substr2);
+		} else {
+			System.out.println("¬ведите слово с четным количеством букв");
+		}
+
+	}
+}
