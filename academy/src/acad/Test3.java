ec25e18c50000a15bd64a6b7c21e374401518c0f
Vla-a <vova.liv.84@mail.ru>
2020-11-11 10:05:41 +0300

    dfh

--- academy/src/acad/Test3.java
+++ academy/src/acad/Test3.java
@@ -3,8 +3,12 @@ package acad;
 public class Test3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String t = "Practice";
+		
+		int s = t.length()/2;
+		String a =t.substring(s-1,s+1);
+System.out.print(a);
 	}
 
 }
